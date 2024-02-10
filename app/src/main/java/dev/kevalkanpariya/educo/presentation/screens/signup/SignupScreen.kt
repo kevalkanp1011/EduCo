6cdbb1f0336d4dce4730c388cb42d29f49939f9f
Keval <kevalkanpariya5051@gmail.com>
2024-02-10 22:26:08 +0530

    SignUpScreen modified with constraint layout

--- app/src/main/java/dev/kevalkanpariya/educo/presentation/screens/signup/SignupScreen.kt
+++ app/src/main/java/dev/kevalkanpariya/educo/presentation/screens/signup/SignupScreen.kt
@@ -9,10 +9,15 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import dev.kevalkanpariya.educo.navigation.Screen
@@ -33,108 +38,199 @@ fun SignupScreen(
     //val isLoading = state.value
     var isLoading by remember { mutableStateOf(false) }
 
-    Column(
-        modifier = Modifier.padding(35.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+    BoxWithConstraints(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 20.dp),
     ) {
-        Spacer(modifier = Modifier.height(45.dp))
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "Welcome!",
-                color = Grey900,
-                fontWeight = FontWeight.Bold,
-                fontSize = 26.sp
-            )
-            Text(
-                text = "Sign up to continue!",
-                color = Grey900,
-                fontWeight = FontWeight.Bold,
-                fontSize = 26.sp
-            )
-        }
-        Spacer(modifier = Modifier.height(60.dp))
-        GoogleButton(backgroundColor = Grey50) {
-            isLoading = true
-            //startForResult.launch(googleSignInClient?.signInIntent)
-        }
-        Spacer(modifier = Modifier.height(20.dp))
-        FaceBookButton(backgroundColor = Grey50) {
-        }
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(text = "or", fontSize = 18.sp, color = Color(0xff585D69))
-        Spacer(modifier = Modifier.height(20.dp))
 
-        Surface(
-            modifier = Modifier
-                .fillMaxWidth(),
-            shape = Shapes.medium,
-            color = Grey50
-        ) {
-            Row(
-                modifier = Modifier
-                    .padding(
-                        start = 12.dp,
-                        end = 16.dp,
-                        top = 12.dp,
-                        bottom = 12.dp
-                    ),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
+        val constraints = if (minWidth < 600.dp) {
+            decoupledConstraints(margin = 16.dp) // Portrait constraints
+        } else {
+            decoupledConstraints(margin = 32.dp) // Landscape constraints
+        }
+
+        ConstraintLayout(constraints) {
+
+            Column(
+                modifier = Modifier.layoutId(titleTextRef),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                TextButton(
-                    onClick = {
-                        navController.navigate("signup_form")
-                    }
+                Text(
+                    text = "Welcome!",
+                    color = Grey900,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 26.sp
+                )
+                Text(
+                    text = "Sign up to continue!",
+                    color = Grey900,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 26.sp
+                )
+            }
+
+            GoogleButton(modifier = Modifier.layoutId(signUpWithGoogleBtnRef), backgroundColor = Grey50) {
+                isLoading = true
+                //startForResult.launch(googleSignInClient?.signInIntent)
+            }
+
+            FaceBookButton(modifier = Modifier.layoutId(signUpWithFacebookBtnRef), backgroundColor = Grey50) {
+            }
+
+            Text(modifier = Modifier.layoutId(orTextRef), text = "or", fontSize = 18.sp, color = Color(0xff585D69), textAlign = TextAlign.Center)
+
+
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .layoutId(signUpWithEmailBtnRef),
+                shape = Shapes.medium,
+                color = Grey50
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(
+                            start = 12.dp,
+                            end = 16.dp,
+                            top = 12.dp,
+                            bottom = 12.dp
+                        ),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
                 ) {
-                    Text(
-                        text = "Sign up with email",
-                        color = Grey900,
-                        fontWeight = FontWeight.SemiBold,
-                        fontSize = 16.sp
-                    )
+                    TextButton(
+                        onClick = {
+                            navController.navigate("signup_form")
+                        }
+                    ) {
+                        Text(
+                            text = "Sign up with email",
+                            color = Grey900,
+                            fontWeight = FontWeight.SemiBold,
+                            fontSize = 16.sp
+                        )
+                    }
                 }
             }
-        }
-        Spacer(modifier = Modifier.height(20.dp))
-        Column(
-            modifier = Modifier.padding(10.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+
             Text(
-                text = "By signing up you are agreed with our",
-                fontSize = 14.sp
+                modifier = Modifier.layoutId(termsAndCondTextRef),
+                text = "By signing up you are agreed with our \nfriendly terms and condition.",
+                fontSize = 14.sp,
+                textAlign = TextAlign.Center
             )
+
             Text(
-                text = "friendly terms and condition.",
-                fontSize = 14.sp
+                modifier = Modifier.layoutId(alreadyHaveAnAcctTextRef),
+                text = "Already have an account?",
+                fontSize = 16.sp,
+                color = Color(0xff383838),
+                textAlign = TextAlign.Center
             )
-        }
-        Spacer(modifier = Modifier.height(77.dp))
-        Text(
-            text = "Already have an account?",
-            fontSize = 16.sp,
-            color = Color(0xff383838)
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        TextButton(onClick = {
-            navController.navigate(Screen.Home.route)
-        }) {
-            Text(
-                text = "Sign in",
-                color = Primary600,
-                fontSize = 16.sp
-            )
-        }
-        errorText?.let {
-            isLoading = false
-            Spacer(modifier = Modifier.height(30.dp))
-            Text(text = it)
+
+            TextButton(
+                modifier = Modifier.layoutId(navigateToSignInBtnRef),
+                onClick = {
+                navController.navigate(Screen.Home.route)
+            }) {
+                Text(
+                    text = "Sign in",
+                    color = Primary600,
+                    fontSize = 16.sp
+                )
+            }
+            errorText?.let {
+                isLoading = false
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(text = it)
+            }
+
         }
 
-    }
+        }
+
+
+
 
 }
 
 
+private fun decoupledConstraints(
+    margin: Dp
+): ConstraintSet {
+    return ConstraintSet {
+
+        val titleText = createRefFor(titleTextRef)
+        val signUpWithGoogleBtn = createRefFor(signUpWithGoogleBtnRef)
+        val signUpWithFacebookBtn = createRefFor(signUpWithFacebookBtnRef)
+        val orText = createRefFor(orTextRef)
+        val signUpWithEmailBtn = createRefFor(signUpWithEmailBtnRef)
+        val termsAndCondText = createRefFor(termsAndCondTextRef)
+        val alreadyHaveAnAcctText = createRefFor(alreadyHaveAnAcctTextRef)
+        val navigateToSignInBtn = createRefFor(navigateToSignInBtnRef)
+
+
+
+
+        constrain(titleText) {
+            top.linkTo(parent.top, 45.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(signUpWithGoogleBtn) {
+            top.linkTo(titleText.bottom, 60.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+
+        }
+
+        constrain(signUpWithFacebookBtn) {
+            top.linkTo(signUpWithGoogleBtn.bottom, 20.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(orText) {
+            top.linkTo(signUpWithFacebookBtn.bottom, 20.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(signUpWithEmailBtn) {
+            top.linkTo(orText.bottom, 20.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(termsAndCondText) {
+            top.linkTo(signUpWithEmailBtn.bottom, 20.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(alreadyHaveAnAcctText) {
+            top.linkTo(termsAndCondText.bottom, 77.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(navigateToSignInBtn) {
+            top.linkTo(alreadyHaveAnAcctText.bottom, 10.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+    }
+}
+
+
+private const val titleTextRef = "title_txt"
+private const val signUpWithGoogleBtnRef = "signup_with_google"
+private const val signUpWithFacebookBtnRef = "signup_with_facebook"
+private const val orTextRef = "or_txt"
+private const val signUpWithEmailBtnRef = "signup_with_email"
+private const val termsAndCondTextRef = "termsAndCondText"
+private const val alreadyHaveAnAcctTextRef = "alreadyHaveAnAcctText"
+private const val navigateToSignInBtnRef = "navigate_to_sign_in_btn"
