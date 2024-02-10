11665f3fc0aa27ea84e52f23d3d2421b0ad37b04
Keval <kevalkanpariya5051@gmail.com>
2024-02-11 01:44:45 +0530

    navigationn added for newly created routes

--- app/src/main/java/dev/kevalkanpariya/educo/navigation/NavGraph.kt
+++ app/src/main/java/dev/kevalkanpariya/educo/navigation/NavGraph.kt
@@ -1,5 +1,6 @@
 package dev.kevalkanpariya.educo.navigation
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -9,7 +10,8 @@ import dev.kevalkanpariya.educo.presentation.screens.AuthScreen
 import dev.kevalkanpariya.educo.presentation.screens.HomeScreen
 import dev.kevalkanpariya.educo.presentation.screens.WelcomeScreen
 import dev.kevalkanpariya.educo.presentation.screens.search.SearchScreen
-import dev.kevalkanpariya.educo.presentation.screens.signin.SigninScreen
+import dev.kevalkanpariya.educo.presentation.screens.signin.SignInScreen
+import dev.kevalkanpariya.educo.presentation.screens.signup.SignUpWithEmailScreen
 import dev.kevalkanpariya.educo.presentation.screens.signup.SignupScreen
 import dev.kevalkanpariya.educo.presentation.viewmodel.SharedViewModel
 
@@ -20,6 +22,32 @@ fun SetUpNavGraph(
 ) {
     val sharedViewModel: SharedViewModel = viewModel()
     NavHost(navController = navController, startDestination = startDestination) {
+
+        composable(Routes.OnboardingScreen.route) {
+            WelcomeScreen(navController = navController, sharedViewModel)
+        }
+
+        composable(Routes.SignInScreen.route) {
+            SignInScreen(onClick = { /*TODO*/ }, errorText = "Account Not Found", navController)
+        }
+
+        composable(Routes.SignUpScreen.route) {
+            SignupScreen(errorText = "Account Not Found", navController)
+        }
+
+        composable(Routes.SignUpWithEmailScreen.route) {
+            SignUpWithEmailScreen()
+        }
+
+        composable(Routes.ResetPasswordScreen.route) {
+
+        }
+
+
+        composable(Routes.HomeScreen.route) {
+            HomeScreen(navController = navController, sharedViewModel = sharedViewModel)
+        }
+
         composable("welcome_screen") {
             WelcomeScreen(navController = navController, sharedViewModel)
         }
@@ -27,7 +55,7 @@ fun SetUpNavGraph(
             HomeScreen(navController = navController, sharedViewModel = sharedViewModel)
         }
         composable("login_screen") {
-            SigninScreen(onClick = { /*TODO*/ }, errorText = "Account Not Found", navController)
+            SignInScreen(onClick = { /*TODO*/ }, errorText = "Account Not Found", navController)
         }
         composable("register_screen") {
             SignupScreen(errorText = "Account Not Found", navController)
