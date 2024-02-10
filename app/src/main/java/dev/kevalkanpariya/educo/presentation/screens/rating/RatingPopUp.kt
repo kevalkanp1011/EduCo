ec200c107435e399fabae4320d193913ef370bd4
Keval <kevalkanpariya5051@gmail.com>
2024-02-11 01:44:20 +0530

    import optimized

--- app/src/main/java/dev/kevalkanpariya/educo/presentation/screens/rating/RatingPopUp.kt
+++ app/src/main/java/dev/kevalkanpariya/educo/presentation/screens/rating/RatingPopUp.kt
@@ -3,6 +3,7 @@ package dev.kevalkanpariya.educo.presentation.screens.rating
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
