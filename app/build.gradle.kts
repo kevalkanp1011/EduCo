6496d3c383382b511475d49c0dc5fb68c7488e24
Keval <kevalkanpariya5051@gmail.com>
2024-02-11 01:45:43 +0530

    compose bom version changed to 2024.02.00 and added android.compose.ui.util dependency get fast api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,13 +60,16 @@ dependencies {
     val accompanist_version = "0.24.3-alpha"
 
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
 
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-util")
     implementation("androidx.compose.ui:ui-tooling-preview")
 
 
@@ -74,8 +77,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
     //Compose Ui Testing
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -86,12 +90,7 @@ dependencies {
 
     // Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.7.7")
-
-    // FOR NAVIGATION
-    //ksp("io.github.raamcosta.compose-destinations:ksp:1.4.2-beta")
-    //implementation("io.github.raamcosta.compose-destinations:core:1.4.2-beta")
-    // END FOR NAVIGATION
-
+    
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
@@ -136,8 +135,6 @@ dependencies {
     // FOR LOGGING
     implementation("com.jakewharton.timber:timber:5.0.1")
 
-    // Constraint Layout
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 
 
 
