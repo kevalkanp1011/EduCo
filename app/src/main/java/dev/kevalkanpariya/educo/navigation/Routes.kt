ce92cae667d6a184244803c4e5d62468d48d5196
Keval <kevalkanpariya5051@gmail.com>
2024-02-10 22:59:22 +0530

    routes added for onboarding and auth feature

---
+++ app/src/main/java/dev/kevalkanpariya/educo/navigation/Routes.kt
@@ -0,0 +1,12 @@
+package dev.kevalkanpariya.educo.navigation
+
+
+sealed class Routes(val route: String) {
+
+    object OnboardingScreen: Routes("onboarding")
+    object SignInScreen: Routes("sign_in")
+    object SignUpScreen: Routes("sign_up")
+    object SignUpWithEmailScreen: Routes("sign_up_with_email")
+    object EnterEmailToGetPasswordScreen: Routes("enter_email_to_get_pwd_screen")
+    object ResetPasswordScreen: Routes("reset_pwd")
+}
