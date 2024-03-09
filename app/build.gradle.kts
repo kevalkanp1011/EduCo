dba4d533742d93cdf5b161381a93beefd0f2d7ae
Keval <kevalkanpariya5051@gmail.com>
2024-03-09 19:07:00 +0530

    necessary changes made

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,22 +59,27 @@ dependencies {
 
     val accompanist_version = "0.24.3-alpha"
 
+    //Core
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-    // Splash API
-    implementation("androidx.core:core-splashscreen:1.0.1")
+
     // Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     // Paging 3.0
     implementation("androidx.paging:paging-compose:3.2.1")
+
     // DataStore Preferences
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     // Palette API
     implementation("androidx.palette:palette-ktx:1.0.0")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
 
+    //Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 
@@ -83,6 +88,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-util")
+    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.runtime:runtime-livedata")
